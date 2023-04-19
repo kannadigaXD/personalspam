d5d5e7829f1ac02216c52b932b6de4b3747d68bd
Kannadiga XD <128951142+kannadigaXD@users.noreply.github.com>
2023-04-20 00:56:23 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -43,11 +43,11 @@ START_MESSAGE = getenv(
     "This is a Powerful Bot Spam Made By [PAPA JI](https://t.me/kannadigaXD)",
 )
 
-PING_PIC = getenv("PING_PIC", "https://telegra.ph/file/8ea21b4a36e8a2ecbd849.jpg")
+PING_PIC = getenv("PING_PIC", "https://graph.org/file/6ed3863209d0be9c968de.jpg")
 
-START_PIC = getenv("START_PIC", "https://telegra.ph/file/8ea21b4a36e8a2ecbd849.jpg")
+START_PIC = getenv("START_PIC", "https://graph.org/file/6ed3863209d0be9c968de.jpg")
 
-HELP_PIC = getenv("HELP_PIC", "https://telegra.ph/file/8ea21b4a36e8a2ecbd849.jpg")
+HELP_PIC = getenv("HELP_PIC", "https://graph.org/file/6ed3863209d0be9c968de.jpg")
 LOG_CHANNEL = getenv("LOG_CHANNEL", None)
 
 HANDLER = getenv("HANDLER", "/")
